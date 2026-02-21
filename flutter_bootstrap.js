af4606287c0b3a60b00602cf99a29e66cca92156
aswanthdsrapha <aswanth@raphacure.com>
2026-02-22 01:16:50 +0530

    feat: Update Flutter web build assets and rephrase professional experience descriptions.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1702276481"
+    serviceWorkerVersion: "2795196899"
   }
 });
